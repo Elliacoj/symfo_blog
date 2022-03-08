f00cb42c3d1b162bd527674cf2422cf386ae9b40
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2022-03-08 16:47:44 +0100

    add file

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Entity\Comment;
+use App\Form\CommentType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+    #[Route('/comment', name: 'app_comment')]
+    public function index(): Response
+    {
+        return $this->render('comment/index.html.twig', [
+            'controller_name' => 'CommentController',
+        ]);
+    }
+
+    #[Route('/article/create_{id}', name: 'app_article_create')]
+    public function addComment(Article $article, Request $request, EntityManagerInterface $entityManager): Response {
+        $comment = new Comment();
+        $comment->addArticle($article)->setAuthor($this->getUser());
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_article', ["id" => $article->getId()]);
+    }
+}
